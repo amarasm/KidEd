121bbc5d53dee42052795e56e52a86a235ae8697
amarasm <48725326+amarasm@users.noreply.github.com>
2019-04-26 12:16:21 +0400

    Update KidEdCat.py

--- KidEdCat.py
+++ KidEdCat.py
@@ -13,8 +13,8 @@ def loadCateg():
     return categ
 
 def AskToChooseACateg(categ):
-    print ("\nDear user, please choose a category from", (": ").join(categ.keys()))
-    chosenCateg = input("Categroy:")
+    print ("\nDear user, please choose a category from", (", ").join(categ.keys()))
+    chosenCateg = input("Category:")
 
     return chosenCateg
 
@@ -25,61 +25,86 @@ def randomCategKey(chosenCateg, categ):
 
     while True:
         if chosenCateg == "animals":
-            print("\nHow many animals do you want to learn today?")
+            print("\nHow many animals do you want to learn today?Your maximum is 25")
             while True:
-                number = int(input("Number of animals you want to learn today:"))
-                if number < 25:
-                    break
+                number = input("Number of animals you want to learn today:")
+                if number.isnumeric():
+                    number = int(number)
+                    if number < 26:
+                        break
+                    else:
+                        print("oops!You have to type a number from 1 to 25!Now try again!")
                 else:
-                    print("oops!,try again!")
-            yourchoices = random.choices(file["categories"]["animals"], k=number)
-            print(yourchoices)
+                    print("Please insert numbers only:)")
+            numberlist = []
+            for keys in file["categories"]["animals"].keys():
+                numberlist.append(keys)
+            yourchoices = random.choices(numberlist, k=number)
+            for number in yourchoices:
+                print("Its name is", number, ", its sound is", file["categories"]["animals"][number]["sound"],"and it's a type of", file["categories"]["animals"][number]["kind"], ".")
             break
 
         elif chosenCateg == "colors":
-            print("\nHow many colors do you want to learn today?")
+            print("\nHow many colors do you want to learn today?Your maximum is 10")
             while True:
-                number = int(input("Number of colors you want to learn today:"))
-                if number < 10:
-                    break
+                number = input("Number of colors you want to learn today:")
+                if number.isnumeric():
+                    number = int(number)
+                    if number < 11:
+                        break
+                    else:
+                        print("oops!You have to type a number from 1 to 10!Now try again!")
                 else:
-                    print("oops!,try again!")
-            yourchoices = random.choices(file["categories"]["colors"], k=number)
-            print(yourchoices)
+                    print("Please insert numbers only:)")
+            numberlist = []
+            for keys in file["categories"]["colors"].keys():
+                numberlist.append(keys)
+            yourchoices = random.choices(numberlist, k=number)
+            for number in yourchoices:
+                print(number)
             break
 
         elif chosenCateg == "alphabet":
-            print("\nHow many letters do you want to learn today?")
+            print("\nHow many letters do you want to learn today?Your maximum is 26")
             while True:
-                number = int(input("Number of letters you want to learn today:"))
-                if number < 26:
-                    break
+                number = input("Number of letters you want to learn today:")
+                if number.isnumeric():
+                    number = int(number)
+                    if number < 27:
+                        break
+                    else:
+                        print("oops!You have to type a number from 1 to 26!Now try again!")
                 else:
-                    print("oops!,try again!")
-            yourchoices = random.choices(file["categories"]["alphabet"], k=number)
-            print(yourchoices)
+                    print("Please insert numbers only:)")
+            numberlist = []
+            for keys in file["categories"]["alphabet"].keys():
+                numberlist.append(keys)
+            yourchoices = random.choices(numberlist, k=number)
+            for number in yourchoices:
+                print("the lowercase is",number, "the uppercase is", file["categories"]["alphabet"][number]["upperCase"], ". For example:", file["categories"]["alphabet"][number]["example"])
             break
 
         elif chosenCateg == "numbers":
+            print("\nHow many numbers do you want to learn today?Your maximum is 10")
             while True:
                 number = input("Number of numbers you want to learn today:")
-                if (number.isnumeric()):
+                if number.isnumeric():
                     number = int(number)
-                    if number < 10:
+                    if number < 11:
                         break
                     else:
-                        print("oops!,try again!")
+                        print("oops!You have to type a number from 1 to 10!Now try again!")
                 else:
                     print("Please insert numbers only")
             numberlist = []
-            for key in file["categories"]["numbers"][0].keys():
+            for key in file["categories"]["numbers"].keys():
                 numberlist.append(key)
             yourchoices = random.choices(numberlist, k=number)
             for number in yourchoices:
-                print(number, " - ", file["categories"]["numbers"][0][number]["name"])
+                print(number, " - ", file["categories"]["numbers"][number]["name"])
             break
         else:
-            print("Oh wait ynumou chose wrong!")
+            print("Oh wait you chose wrong!")
             chosenCateg = AskToChooseACateg(categ)
 
 
