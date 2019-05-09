25b603dbb471029fb6a99204ed4dd2492f283e52
amarasm <48725326+amarasm@users.noreply.github.com>
2019-05-09 22:27:03 +0400

    Update KidEdNewCat.py

--- KidEdNewCat.py
+++ KidEdNewCat.py
@@ -3,23 +3,15 @@ import json
 def loadCateg():
     with open('Categories.json') as data_file:
         file = json.load(data_file)
+    return file
+
+def printLengthOfTheCategory(categ):
+    print("\nThe length of the category is currently", len(categ["categories"]))
 
 
-    categ = file["categories"]
-    return categ
-
-def length(categ):
-    print("\nThe length of the category is currently", len(categ))
-
-def saveNewCateg(newTopic):
-    data_file = open("categories.json", "w")
-    data_file.write(json.dumps(newTopic))
-    data_file.close()
 
 
-def additionToCateg(categ):
-    with open('Categories.json') as data_file:
-        file = json.load(data_file)
+def additionToCateg(file):
 
     while True:
         Addition = input("\nDear programmer do you want to add another category to the current one? Please say either yes or no")
@@ -38,21 +30,19 @@ def additionToCateg(categ):
                     newTopicKey = input("Please tell me the key")
                     newTopic["name"] = input("Please add the display name")
                     newCategory[newTopicKey] = newTopic
-                    saveNewCategory = saveNewCateg(newCategory[newTopicKey])
+                    data_file = open("categories.json", "w")
+                    data_file.write(json.dumps(file))
+                    data_file.close()
         else:
             print("Nope, please write yes or no! It's simple :D")
-    return newTopic
+    return file
 
-
-
-def currLength(length):
-    print(length)
 
 def main():
     loadCategory = loadCateg()
-    lengthOfTheCategory = length(loadCategory)
-    addNewCategory = additionToCateg(loadCategory)
+    printLengthOfTheCategory(loadCategory)
+    newFile = additionToCateg(loadCategory)
 
-    currLength(lengthOfTheCategory)
+    printLengthOfTheCategory(newFile)
 
 main()
